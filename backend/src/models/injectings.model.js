5572529ce862b981f80b10996c960ff1c8fca096
lagmanzaza <lagmanmc@gmail.com>
2018-06-26 22:26:31 +0700

    [Mongoose] medicine injecting model

--- backend/src/models/injectings.model.js
+++ backend/src/models/injectings.model.js
@@ -1,14 +1,34 @@
 // injectings-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const injectings = new Schema({
-    text: { type: String, required: true }
+    userId: {
+      type: String,
+      required: true,
+    },
+    injuryDate: {
+      type: Date,
+      required: true,
+    },
+    injectionDate: {
+      type: Date,
+      required: true,
+    },
+    injuryparts: {
+      type: Array,
+      required: true,
+    },
+    remark: {
+      type: String,
+      default: '',
+    },
   }, {
-    timestamps: true
+    timestamps: true,
+    versionKey: false,
   });
 
   return mongooseClient.model('injectings', injectings);
