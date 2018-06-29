5f74f28b4b7bba03ea34f10b5f185aa619f023f0
lagmanzaza <lagmanmc@gmail.com>
2018-06-29 18:15:26 +0700

    [Component]  sign up information

--- frontend/src/pages/SignUp.vue
+++ frontend/src/pages/SignUp.vue
@@ -19,6 +19,63 @@
         <input type="password" v-model="password">
       </div>
     </div>
+    <div class="row ignore-screen level">
+      <div class="col-3 ignore-screen level-item no-space">
+        <p class="font-maitree">รหัสผ่าน:</p>
+      </div>
+      <div class="col-3 ignore-screen level-item no-space">
+        <input type="password" v-model="password">
+      </div>
+    </div>
+    <div class="row ignore-screen level">
+      <div class="col-3 ignore-screen level-item">
+        <p class="font-maitree">วันเดือนปีเกิด</p>
+      </div>
+      <div class="col-9 ignore-screen level-item">
+        <!-- <input type="password" v-model="password"> -->
+        <datepicker :language="th" :input-class="'calendar'" placeholder="วันเกิด" v-model="birthDate"></datepicker>
+      </div>
+    </div>
+    <div class="row ignore-screen level">
+      <div class="col-3 ignore-screen level-item">
+        <p class="font-maitree">รหัสบัตรประชาชน</p>
+      </div>
+      <div class="col-9 ignore-screen level-item">
+        <input type="text" v-model="password">
+      </div>
+    </div>
+    <div class="row ignore-screen level">
+      <div class="col-3 ignore-screen level-item">
+        <p class="font-maitree">หมายเลขโทรศัพท์</p>
+      </div>
+      <div class="col-9 ignore-screen level-item">
+        <input type="text" v-model="password">
+      </div>
+    </div>
+    <div class="row ignore-screen level">
+      <div class="col-3 ignore-screen level-item">
+        <p class="font-maitree">โรงพยาบาล</p>
+      </div>
+      <div class="col-9 ignore-screen level-item">
+        <input type="text" v-model="password">
+      </div>
+    </div>
+    <div class="row ignore-screen level">
+      <div class="col-3 ignore-screen level-item">
+        <p class="font-maitree">แพทย์ที่รักษา</p>
+      </div>
+      <div class="col-9 ignore-screen level-item">
+        <input type="password" v-model="password">
+      </div>
+    </div>
+    <div class="row ignore-screen level">
+      <div class="col-3 ignore-screen level-item">
+        <p class="font-maitree">สิทธิ์การรักษา</p>
+      </div>
+      <div class="col-9 ignore-screen level-item">
+        <input type="password" v-model="password">
+      </div>
+    </div>
     <div class="col-12">
       <button class="btn font-maitree" @click="signUp()">สมัครสมาชิก</button>
       <br>
@@ -31,11 +88,25 @@
   import {
     userService
   } from '../plugins/feathers.js'
+  import Datepicker from 'vuejs-datepicker';
+  import {
+    th
+  } from 'vuejs-datepicker/dist/locale'
   export default {
+    components: {
+      Datepicker
+    },
     data: function () {
       return {
+        th: th,
         username: '',
         password: '',
+        id: '',
+        telePhoneNumber: '',
+        hospitalName: '',
+        doctorName: '',
+        birthDate: '',
+        sex:'',
         err: ''
       }
     },
@@ -65,6 +136,10 @@
     padding-bottom: 10%;
   }
 
+  .calendar {
+    width: 100vw;
+  }
+
   img {
     display: block;
     margin-left: auto;
@@ -72,12 +147,12 @@
   }
 
   input {
-    font-size: 15px;
+    font-size: 18px;
   }
 
   .container {
-    width: 40vw;
-    margin-left: 30vw;
+    width: 50vw;
+    margin-left: 20vw;
     padding-top: 10vh;
   }
 
