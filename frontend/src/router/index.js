32501b0242904c1ea58a5d0ccef941bc5baf9f30
lagmanzaza <lagmanmc@gmail.com>
2018-07-04 03:20:54 +0700

    [Router] injury route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import SignUpPage from '../pages/SignUp.vue';
 import SignInPage from '../pages/SignIn.vue';
 import DashboardPage from '../pages/dashboard.vue';
 import InjectionPage from '../pages/injection.vue';
+import InjuryPage from '../pages/injury.vue';
 Vue.use(Router)
 
 const router = new Router({
@@ -22,10 +23,16 @@ const router = new Router({
       path: '/dashboard',
       name: 'dasboard',
       component: DashboardPage
-    }, {
+    }, 
+    {
       path: '/injection',
       name: 'injection',
       component: InjectionPage
+    },
+    {
+      path: '/injury',
+      name: 'injury',
+      component: InjuryPage
     }
   ]
 })
