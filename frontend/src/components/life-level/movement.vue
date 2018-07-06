aaa0ad456351202cf12f749629ccf215ec807a98
lagmanzaza <lagmanmc@gmail.com>
2018-07-07 03:04:44 +0700

    [Component] life quality

---
+++ frontend/src/components/life-level/movement.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <p v-if="level === 1">ไม่มีปัญหาในการเดิน</p>
+  <p v-if="level === 2">มีปัญหาในการเดินเล็กน้อย</p>
+  <p v-if="level === 3">มีปัญหาในการเดินปานกลาง</p>
+  <p v-if="level === 4">มีปัญหาในการเดินอย่างมาก</p>
+  <p v-if="level === 5">ไม่สามารถเดินได้</p>
+</div>
+</template>
+<script>
+export default {
+  props: ['level']
+}
+</script>
