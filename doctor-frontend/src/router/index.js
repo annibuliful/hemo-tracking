4a775ae643aac82c7ba4608a6baef6dcee358655
Thanakorn Pasangthren <thanakorn@code-app.com>
2018-07-08 01:09:53 +0700

    register page

--- doctor-frontend/src/router/index.js
+++ doctor-frontend/src/router/index.js
@@ -1,15 +1,14 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+import SignUpPage from "../pages/Signup.vue";
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: "/",
+      name: "Hello",
+      component: SignUpPage
     }
   ]
-})
+});
