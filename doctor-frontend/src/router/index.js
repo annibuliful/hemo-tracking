58f4a86b6593b6396e907555ca7dc4eef9886ffb
lagmanzaza <lagmanmc@gmail.com>
2018-07-11 18:47:13 +0700

    [Router Guard, Component] authentication

--- doctor-frontend/src/router/index.js
+++ doctor-frontend/src/router/index.js
@@ -1,20 +1,48 @@
 import Vue from "vue";
 import Router from "vue-router";
+import feathers from "../plugins/feathers.js";
 import SignUpPage from "../pages/Signup.vue";
 import SignInPage from "../pages/Signin.vue";
+import DashboardPage from "../pages/Dashboard.vue";
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: "/",
-      name: "Signin",
+      name: "signin",
       component: SignInPage
     },
     {
       path: "/reg",
-      name: "Signup",
+      name: "signup",
       component: SignUpPage
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardPage
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.name === 'signin' || to.name === 'signup') {
+    feathers.authenticate().then(()=>{
+      next('/dashboard');
+    }).catch((err) => {
+      next(true)
+    })
+
+  } else {
+    feathers.authenticate().then(() => {
+      next(true)
+    }).catch((err) => {
+      next('/')
+    })
+
+  }
+
+
+})
+export default router
